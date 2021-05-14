3d03c3f47faa73186559a094951eb1ec350a9ee0
Bahtiyar <bahtiyar_a@live.com>
2021-05-14 22:13:27 +0800

    Made core functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import TextareaAutosize from "react-autosize-textarea"
+import { Controlled as CodeMirror } from "react-codemirror2"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+require("codemirror/lib/codemirror.css")
+require("codemirror/theme/material.css")
+require("codemirror/theme/neat.css")
+require("codemirror/mode/xml/xml.js")
+require("codemirror/mode/javascript/javascript.js")
+
+function TextareaOl() {
+    const [value, setValue] = useState("")
+    const options = {
+        lineNumbers: true,
+        lineWrapping: true,
+        lineNumberFormatter: lineNumberFormatter,
+    }
+    return (
+        // <div className="textarea-ol">
+        //     <div className="numbers">
+        //         1<br />
+        //         2<br />
+        //         3<br />
+        //     </div>
+        //     <TextareaAutosize
+        //         placeholder="Add content instance, one line each"
+        //         className="textarea"
+        //         value={value}
+        //         onChange={(e) => handleChange(e, setValue)}
+        //     />
+        // </div>
+        <CodeMirror
+            value={value}
+            options={options}
+            onBeforeChange={(editor, data, value) => {
+                setValue(value)
+            }}
+            onChange={(editor, data, value) => {
+                // console.log(value)
+            }}
+        />
+    )
+
+    function lineNumberFormatter(line) {
+        const arr = value.split("\n")
+        var blankLine = 0
+
+        if (arr[line - 1] === "") {
+            return ""
+        }
+
+        for (let i = 0; i < line - 1; i++) {
+            const element = arr[i]
+            if (element === "") {
+                blankLine++
+            }
+        }
+
+        return line - blankLine
+    }
 }
 
-export default App;
+function handleChange(e, setValue) {
+    setValue(e.target.value)
+    console.log(e.target.value)
+}
+
+export default TextareaOl
